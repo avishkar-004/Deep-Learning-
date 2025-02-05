869c4ceb1083fce1d76ae1a63b37e4576b7c0547
Avishkar Pawar <avishkarpawar004@gmail.com>
2025-02-05 14:45:00 +0530

    feat: add Transformer components with multi-head attention

---
+++ utils/transformer.py
@@ -0,0 +1,51 @@
+"""Transformer architecture components."""
+
+import tensorflow as tf
+from tensorflow.keras import layers
+import numpy as np
+
+
+def positional_encoding(position, d_model):
+    """Generate positional encoding."""
+    angle_rads = np.arange(position)[:, np.newaxis] / np.power(
+        10000, (2 * (np.arange(d_model)[np.newaxis, :] // 2)) / d_model
+    )
+    angle_rads[:, 0::2] = np.sin(angle_rads[:, 0::2])
+    angle_rads[:, 1::2] = np.cos(angle_rads[:, 1::2])
+    return tf.cast(angle_rads[np.newaxis, :, :], dtype=tf.float32)
+
+
+class MultiHeadAttention(layers.Layer):
+    """Multi-head attention layer."""
+
+    def __init__(self, d_model, num_heads):
+        super().__init__()
+        self.num_heads = num_heads
+        self.d_model = d_model
+        self.depth = d_model // num_heads
+
+        self.wq = layers.Dense(d_model)
+        self.wk = layers.Dense(d_model)
+        self.wv = layers.Dense(d_model)
+        self.dense = layers.Dense(d_model)
+
+    def split_heads(self, x, batch_size):
+        x = tf.reshape(x, (batch_size, -1, self.num_heads, self.depth))
+        return tf.transpose(x, perm=[0, 2, 1, 3])
+
+    def call(self, v, k, q, mask=None):
+        batch_size = tf.shape(q)[0]
+        q = self.split_heads(self.wq(q), batch_size)
+        k = self.split_heads(self.wk(k), batch_size)
+        v = self.split_heads(self.wv(v), batch_size)
+
+        scaled_attention = tf.matmul(q, k, transpose_b=True) / tf.math.sqrt(
+            tf.cast(self.depth, tf.float32)
+        )
+        if mask is not None:
+            scaled_attention += (mask * -1e9)
+        weights = tf.nn.softmax(scaled_attention, axis=-1)
+        output = tf.matmul(weights, v)
+        output = tf.transpose(output, perm=[0, 2, 1, 3])
+        output = tf.reshape(output, (batch_size, -1, self.d_model))
+        return self.dense(output), weights
