d5c1c4e599cf12a29e0b68a20e517ec0226d9d1e
Avishkar Pawar <avishkarpawar004@gmail.com>
2025-02-20 09:30:00 +0530

    chore: add setup.py for package structure

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="deep-learning-assignments",
+    version="1.0.0",
+    description="Deep Learning Lab Assignment Collection",
+    author="Avishkar Pawar",
+    packages=find_packages(),
+    python_requires=">=3.8",
+)
